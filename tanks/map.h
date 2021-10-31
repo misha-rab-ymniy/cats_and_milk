a51f0a21235057e918bf8fe62bcbb9d29dc9f647
jenshenJ <91986803+jenshenJ@users.noreply.github.com>
2021-10-31 21:27:01 +0300

    Add files via upload

---
+++ tanks/map.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML\Graphics.hpp>
+#include <string>
+
+const int HEIGHT_MAP = 10;
+const int WIDTH_MAP = 19;
+
+sf::String TileMap[HEIGHT_MAP] = {
+	"                   ",
+	"     sss    s      ",
+	"    s      s s     ",
+	"    s     s   s    ",
+	"    s    sssssss   ",
+	"     sss s     s   ",
+	"                   ",
+	"      s            ",
+	"                   ",
+	"                   ",
+};
